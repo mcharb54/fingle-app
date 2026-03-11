0f190f952c573dfca84aa5845a2127c63d0d8fe1
mcharb54 <mcharb54@gmail.com>
2026-03-10 21:47:18 -0400

    Update usePushNotifications.ts

--- frontend/src/hooks/usePushNotifications.ts
+++ frontend/src/hooks/usePushNotifications.ts
@@ -4,11 +4,15 @@ import { pushApi } from '../api'
 
 const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY as string | undefined
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = atob(base64)
-  return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)))
+  const output = new Uint8Array(rawData.length)
+  for (let i = 0; i < rawData.length; i++) {
+    output[i] = rawData.charCodeAt(i)
+  }
+  return output
 }
 
 export function usePushNotifications() {
